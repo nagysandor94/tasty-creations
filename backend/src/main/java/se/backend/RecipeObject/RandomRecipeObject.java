3d48e38a5e07a8f0b4a966a46abeface49acfd3b
nagysandor94 <119703174+nagysandor94@users.noreply.github.com>
2023-03-30 09:58:50 +0200

    update API

--- backend/src/main/java/se/backend/RecipeObject/RandomRecipeObject.java
+++ backend/src/main/java/se/backend/RecipeObject/RandomRecipeObject.java
@@ -1,10 +1,12 @@
 package se.backend.RecipeObject;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RandomRecipeObject{
         public ArrayList<Recipe> getRecipes() {
                 return recipes;
