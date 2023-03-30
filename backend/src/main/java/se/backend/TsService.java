ec577087a32d10b460976c2463753ff22efe03ac
nagysandor94 <119703174+nagysandor94@users.noreply.github.com>
2023-03-30 19:44:56 +0200

    Update TsService.java

--- backend/src/main/java/se/backend/TsService.java
+++ backend/src/main/java/se/backend/TsService.java
@@ -122,7 +122,6 @@ public class TsService {
 
 
         HttpResponse<String> jsonString = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-
         
         List<ResponseSearchByName> responseSearchByNameList = mapper.readValue(jsonString.body(), new TypeReference<List<ResponseSearchByName>>(){});
 
